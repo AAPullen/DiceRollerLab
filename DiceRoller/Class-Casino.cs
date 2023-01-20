b9c4cc37725072f0d86507c7c246cdf79d703ec8
Andrew <andrewpullen1@gmail.com>
2023-01-19 21:48:26 -0500

    Second Commit

---
+++ DiceRoller/Class-Casino.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public static class Casino
+    {
+        public static int DiceRoll(int diceSides)
+        {
+            Random roll = new Random();
+            int result = roll.Next(1 , (diceSides + 1));
+            return result;
+        }
+
+        public static string Reroll()
+        {
+            Console.Write("Would you like to roll again (y/n)? ");
+            string answer = Console.ReadLine().ToLower().Trim();
+            return answer;
+        }
+        
+        public static string SixSidedComboCheck(int dieOne, int dieTwo)
+        {
+            int total = dieOne + dieTwo;
+
+            if (dieOne == 1 && dieTwo == 1)
+            {
+                return "Snake Eyes.";
+            }
+            else if (dieOne == 6 && dieTwo == 6)
+            {
+                return "Box Cars.";
+            }
+            else if ((dieOne == 1 && dieTwo == 2) || (dieOne == 2 && dieTwo == 1))
+            {
+                return "Ace Deuce.";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string CheckWinLose(int dieOne, int dieTwo)
+        {
+            int total = dieOne + dieTwo;
+            if (total == 2 || total == 3 || total == 12)
+            {
+                return "Craps.\n";
+            }
+            else if (total == 7 || total == 11)
+            {
+                return "you Win!\n";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,39 @@
+﻿using DiceRoller;
+
+
+Console.WriteLine("Welcome to the Grand Circus Casino!\n" +
+    "How many sides should the pair of dice have?");
+
+bool playGame = false;
+while (playGame == false)
+{ 
+    string userSides = Console.ReadLine();
+    if (int.TryParse(userSides, out int diceSides))
+    {
+        playGame = true;
+        string reroll;
+
+        do
+        {
+            int dieOne = Casino.DiceRoll(diceSides);
+            int dieTwo = Casino.DiceRoll(diceSides);
+            Console.WriteLine($"Results = {dieOne} and {dieTwo}. Total = {dieOne + dieTwo}.");
+
+            if (diceSides == 6)
+            {
+                string Combos = Casino.SixSidedComboCheck(dieOne, dieTwo);
+                Console.WriteLine(Combos);
+
+                string winLose = Casino.CheckWinLose(dieOne, dieTwo);
+                Console.WriteLine(winLose);
+            }
+
+            reroll = Casino.Reroll();
+
+        } while (reroll == "y");
+    }
+    else
+    {
+        Console.WriteLine("You have made an invalid entry for number of sides. Please enter an integer.");
+    }
+}
